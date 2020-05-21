5e96827db171be04754690e7c73f1289eba58a90
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-05-21 19:52:40 +0100

    u

--- challenge-high-score-tables/my-app/src/App.js
+++ challenge-high-score-tables/my-app/src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Scoreboard from './Scoreboard';
 import allCountryScores from './scores';
 import './App.css';
 
-
 function App() {
-
+  const [count, setCount] = useState(0);
   function handleButtonClick (){
-    document.body.style.backgroundColor="red"
+    setCount (count + 1)
   }
 
   return (
     <div className="App">
       <h1>High Scores per Country</h1>
-        <button name="button" className="button" onClick={handleButtonClick}> BUTTON
+        <button name="button" className="button" onClick={handleButtonClick}> {count}
       </button>
         {allCountryScores
           .sort((a, b) => a.name.localeCompare(b.name))
