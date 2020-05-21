445f982cbfecd5a34fd791641ffdb39307f1e3a9
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-05-21 19:41:43 +0100

    update

--- challenge-high-score-tables/my-app/src/App.js
+++ challenge-high-score-tables/my-app/src/App.js
@@ -3,10 +3,18 @@ import Scoreboard from './Scoreboard';
 import allCountryScores from './scores';
 import './App.css';
 
+
 function App() {
+
+  function handleButtonClick (){
+    document.body.style.backgroundColor="red"
+  }
+
   return (
     <div className="App">
       <h1>High Scores per Country</h1>
+        <button name="button" className="button" onClick={handleButtonClick}> BUTTON
+      </button>
         {allCountryScores
           .sort((a, b) => a.name.localeCompare(b.name))
           .map(element => <Scoreboard
